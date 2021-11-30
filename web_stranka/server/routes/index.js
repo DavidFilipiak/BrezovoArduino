cece25b52e3af38368178bf158616d30f6f23185
David Filipiak <df239@canterbury.ac.uk>
2021-11-30 10:39:02 +0000

    server prijíma POST požiadavky

--- web_stranka/server/routes/index.js
+++ web_stranka/server/routes/index.js
@@ -1,12 +1,14 @@
 var express = require('express');
 var router = express.Router();
-
+ /*
 var sprava = {
   cerpadlo: false,
   spaliny: 19,
   teplotyL: [{ id: 1, t: 18.1 }, { id: 2, t: 17.7}, { id: 3, t: 19.4 }, { id: 4, t: 19.2 }],
   teplotyR: [{ id: 1, t: 18.4 }, { id: 2, t: 19.4 }, { id: 3, t: 18.6 }, { id: 4, t: 19.4 }]
-}
+}*/
+
+var sprava ={};
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -14,6 +16,25 @@ router.get('/', function(req, res, next) {
     title: 'Arduino',
     msg: sprava
   });
+});
+
+/* POST */
+router.post('/', function(req, res){
+  var body = req.body;
+  sprava.cerpadlo = body.cerpadlo;
+  sprava.spaliny = body.spaliny;
+  arrL = [];
+  for (let i = 0; i < body.teplotyL.length; i++){
+    arrL.push({id: i+1, t: body.teplotyL[i]});
+  }
+  sprava.teplotyL = arrL;
+  arrR = [];
+  for (let i = 0; i < body.teplotyR.length; i++) {
+    arrL.push({ id: i + 1, t: body.teplotyR[i] });
+  }
+  sprava.teplotyR = arrR;
+
+  res.status(201).send("good");
 });
 
 module.exports = router;
