b0f893d5b63e449fe5a72302583418e350083b9b
David Filipiak <df239@canterbury.ac.uk>
2021-11-30 09:47:31 +0000

    stranka dokaze zobrazit cerpadlo a spaliny

--- web_stranka/server/routes/index.js
+++ web_stranka/server/routes/index.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
 
+var sprava = {
+  cerpadlo: false,
+  spaliny: 19,
+  teplotyL: [{ id: 1, t: 18.1 }, { id: 2, t: 17.7}, { id: 3, t: 19.4 }, { id: 4, t: 19.2 }],
+  teplotyR: [{ id: 1, t: 18.4 }, { id: 2, t: 19.4 }, { id: 3, t: 18.6 }, { id: 4, t: 19.4 }]
+}
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { 
+    title: 'Arduino',
+    msg: sprava
+  });
 });
 
 module.exports = router;
